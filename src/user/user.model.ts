0f031076562aa22f11cacbf7b34cddf5a78708b3
Herman Streltsov <mail@streltsov.dev>
2022-08-05 21:45:41 +0300

    Add id & timestamps to user model

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,6 +1,15 @@
-import { prop } from "@typegoose/typegoose";
+import { prop, modelOptions } from "@typegoose/typegoose";
 
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+})
 export class User {
+  id: string;
+
   @prop({ required: true })
   email!: string;
 
